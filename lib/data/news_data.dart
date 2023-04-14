3613caac47839b171b0134733b61cda374fabd14
amyu16 <pipilemak1@gmail.com>
2023-04-14 23:07:15 +0700

    Ini bagian ku boss

---
+++ lib/data/news_data.dart
@@ -0,0 +1,61 @@
+import '/models/news.dart';
+
+///[* data berita]
+List<News> newsData = [
+  News(
+    id: 1,
+
+    ///[* id berita]
+    title: "Mudik Temu Kangen Bersama Keluarga",
+
+    ///[* judul berita]
+    description:
+        "Menjalin silaturahmi antar keluarga, sahabat, dan tetangga di kampung halaman. Membawa dampak ekonomi mikro, seperti uang dan barang kepada keluarga di kampung halaman. Menjaga tali persaudaraan dan persatuan. Memberikan pengalaman kultural dan agama yang sifatnya positif..",
+
+    ///[* isi berita]
+    image: "https://i.postimg.cc/FKn4RkJD/download.jpg",
+
+    /// [* photo berita]
+  ),
+  News(
+    id: 2,
+    title: "Hati-Hati Sedang Mudik Cuaca Tidak Menentu",
+    description:
+        "Para pemudik harus berhati-hati dengan ancaman gangguan perjalanan akibat curah hujan tinggi pada musin Mudik Lebaran 2022. Sekretaris Badan Geologi Kementerian ESDM Ediar Usman mengatakan, ada potensi tren peningkatan curah hujan. ",
+    image:
+        "https://i.postimg.cc/vHR3y8D9/057253300-1611224755-Whats-App-Image-2021-01-21-at-17-21-13.jpg",
+  ),
+  News(
+    id: 3,
+    title: "Tips And Trick Mudik",
+    description:
+        "Sejak telah ditetapkannya hari libur nasional perayaan Idul Fitri 1443 H dan cuti bersama , yaitu libur hari raya lebaran akan jatuh pada tanggal 2-3 Mei 2022, sementara cuti bersama akan berlangsung pada tanggal 29 April kemudian berlanjut pada tanggal 4-6 Mei 2022.",
+    image: "https://i.postimg.cc/zB7bcjL0/download-1.jpg",
+  ),
+  News(
+    id: 4,
+    title: "Rute Tercepat Mudik",
+    description:
+        "Momen mudik lebaran yang ditunggu-tunggu akahirnya segera tiba. Momen yang satu ini memang terasa sangat istimewa. Pasalnya pandemi yang hadir hampir dua tahun belakangan, membatasi kita semua untuk menjalankan hal yang sudah jadi tradisi ini.",
+    image: "https://i.postimg.cc/43DRTkc3/download-2.jpg",
+  ),
+];
+
+///[* berita terpopuler]
+List<News> hotNews = [
+  News(
+    id: 4,
+    title: "Rute Tercepat Mudik",
+    description:
+        "Momen mudik lebaran yang ditunggu-tunggu akahirnya segera tiba. Momen yang satu ini memang terasa sangat istimewa. Pasalnya pandemi yang hadir hampir dua tahun belakangan, membatasi kita semua untuk menjalankan hal yang sudah jadi tradisi ini.",
+    image: "https://i.postimg.cc/43DRTkc3/download-2.jpg",
+  ),
+  News(
+    id: 2,
+    title: "Hati-Hati Sedang Mudik Cuaca Tidak Menentu",
+    description:
+        "Para pemudik harus berhati-hati dengan ancaman gangguan perjalanan akibat curah hujan tinggi pada musin Mudik Lebaran 2022. Sekretaris Badan Geologi Kementerian ESDM Ediar Usman mengatakan, ada potensi tren peningkatan curah hujan. ",
+    image:
+        "https://i.postimg.cc/vHR3y8D9/057253300-1611224755-Whats-App-Image-2021-01-21-at-17-21-13.jpg",
+  ),
+];
