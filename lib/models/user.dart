fef69ceefb3c3a5655c6def7f0b63e7258103550
ZidanRakanR <120114859+ZidanRakanR@users.noreply.github.com>
2023-04-14 23:15:47 +0700

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,40 +1,29 @@
 class User {
   final int id;
   final String name;
-  final String username;
+  final String userName;
   final String email;
-  final String? profilePhoto;
+  final String? profileImage;
   final String? phoneNumber;
 
   User({
     required this.id,
     required this.name,
-    required this.username,
+    required this.userName,
     required this.email,
-    this.profilePhoto,
+    this.profileImage,
     this.phoneNumber,
   });
 
-  factory User.fromMap(Map<String, dynamic> map) {
-    return User(
-      id: map["id"],
-      name: map["name"],
-      username: map["username"],
-      email: map["email"],
-      profilePhoto: map["profile_photo"],
-      phoneNumber: map["phone_number"],
-    );
-  }
-
+  ///[* data profile]
   factory User.dummy() {
     return User(
-      id: 001,
-      name: "Diar Nur Rizky",
-      username: "diarn",
-      email: "diarn@email.com",
-      profilePhoto:
-          "https://cdn.idntimes.com/content-images/community/2020/09/104967619-2451390651819718-4934284928595035666-n-2fc82b7325949c7d003c1c58e17d48b7.jpg",
-      phoneNumber: "085123456789",
+      id: 027,
+      name: "Zidan Rakan",
+      userName: "Zrr02",
+      email: "2006078@itg.ac.id",
+      profileImage: "https://i.postimg.cc/Wbc5MpG8/jefri-nichol-1-43.jpg",
+      phoneNumber: "081511548595",
     );
   }
 }
